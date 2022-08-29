35c2b387fd84869d5a1cb1b04fc1c23739a0558f
john <jrborges52@gmail.com>
2022-08-29 12:18:12 -0700

    add he break feature to render

--- client/src/components/ShowBreak.jsx
+++ client/src/components/ShowBreak.jsx
@@ -1,10 +1,43 @@
+import { useEffect } from "react"
+import { useState } from "react"
 
-export const ShowBreak = ({value}) => {
+export const ShowBreak = () => {
+
+  const [second, setSecond] = useState(0)
+  const [minute, setMinute] = useState(5)
+  
+  useEffect(()=> {
+
+    const interval = setTimeout(() => {
+      
+      if (second > 0) {
+        setSecond(second -1)
+
+      }
+      if (second === 0 ) {
+        setMinute(minute - 1)
+        setSecond(59)
+        
+        
+        
+      }
+    }, 300)
+
+    if (minute === 0 && second === 0) {
+      console.log("ACABOU BREAK")
+      clearTimeout(interval)
+    }
+    
+
+  }, [])
+
+
+
 
 
   return (
     <div>
-      <span> we will figured it out this after </span>
+      <span> Enjoy your break</span>
     </div>
 
   )
--- client/src/components/Timer.jsx
+++ client/src/components/Timer.jsx
@@ -9,46 +9,49 @@ import { useEffect } from 'react'
 
 export const Timer = () => {
 
-  
-  
-
   const [minute, setMinute] = useState(1)
   const [second, setSecond] = useState(11)
   const [start, setStart] = useState(false)
+  const [isBreak, setIsBreak] = useState(false)
 
-      if(start ===true ){
+    if(start === true ){
 
-        const interval = setTimeout(()=>{
-          
-          if (second > 0 )  {
-            setSecond(second - 1)
-          }
-          if (second === 0 ) {
-            setMinute(minute - 1)
-            setSecond(11)
-          }
-          
-        },500)
+      const interval = setTimeout(()=>{
         
-        
-        if (minute === 0 && second === 0 ) {
-          console.log('ACABOU TUDO')
-          clearTimeout(interval)
-          setMinute(1)
-          setSecond(11)
-          setStart(false)
+        if (second > 0 )  {
+          setSecond(second - 1)
         }
+        if (second === 0 ) {
+          setMinute(minute - 1)
+          setSecond(11)
+        }
+        
+      },500)
+      
+      
+      if (minute === 0 && second === 0 ) {
+        console.log('ACABOU TUDO')
+        clearTimeout(interval)
+        setMinute(1)
+        setSecond(11)
+        setStart(false)
+        setIsBreak(true)
       }
-     
-    
+    }
     
     return (
+      <>
+
+      {!isBreak ? 
       <ShowPomodoro 
       minutes={minute}
       seconds={second}
-      onClick={() => (setStart(true))} 
+      onClick={() => (setStart(true), setIsBreak(false)) } 
       />
+      :
+      <ShowBreak />
+    }
+      </>
       );
-   
-      
+       
     }
