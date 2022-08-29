cf6ea63667304fac5ccacc8eacd833b2f184cffc
john <jrborges52@gmail.com>
2022-08-29 11:49:38 -0700

    take the useEffect out to not render when mounting

--- client/src/components/Timer.jsx
+++ client/src/components/Timer.jsx
@@ -9,49 +9,46 @@ import { useEffect } from 'react'
 
 export const Timer = () => {
 
-
+  
+  
 
   const [minute, setMinute] = useState(1)
   const [second, setSecond] = useState(11)
+  const [start, setStart] = useState(false)
 
+      if(start ===true ){
 
-  useEffect(()=>{
-
-    const interval= setTimeout(()=>{
-      
-      if (second > 0 )  {
-        setSecond(second - 1)
+        const interval = setTimeout(()=>{
+          
+          if (second > 0 )  {
+            setSecond(second - 1)
+          }
+          if (second === 0 ) {
+            setMinute(minute - 1)
+            setSecond(11)
+          }
+          
+        },500)
+        
+        
+        if (minute === 0 && second === 0 ) {
+          console.log('ACABOU TUDO')
+          clearTimeout(interval)
+          setMinute(1)
+          setSecond(11)
+          setStart(false)
+        }
       }
-      if (second === 0 ) {
-        setMinute(minute - 1)
-        setSecond(11)
-      }
-      
-    },500)
-
-
-    if (minute === 0 && second === 0 ) {
-      console.log('ACABOU TUDO')
-      clearTimeout(interval)
-    }
-
-
-  },[{minute, second}])
-
-
-
-  // if (minutes + seconds <= 0) {
-  //   return <ShowBreak />
-
-  // } else {
+     
+    
+    
     return (
       <ShowPomodoro 
-        minutes={minute}
-        seconds={second}
-        onClick={()=>{console.log('hey')}}
-        
-        
+      minutes={minute}
+      seconds={second}
+      onClick={() => (setStart(true))} 
       />
-    );
-  // }
-}
+      );
+   
+      
+    }
