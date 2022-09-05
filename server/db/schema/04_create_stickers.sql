3d40ebf8597d9aba456039528deb0e900593e41f
john <jrborges52@gmail.com>
2022-09-05 15:58:30 -0700

    add stickers to the page

--- server/db/schema/04_create_stickers.sql
+++ server/db/schema/04_create_stickers.sql
@@ -3,6 +3,7 @@ DROP TABLE IF EXISTS stickers CASCADE;
 -- CREATE STICKERS
 CREATE TABLE stickers (
   id SERIAL PRIMARY KEY,
+  title VARCHAR(255) NOT NULL,
   stickerPic text NOT NULL
   );
   
--- server/db/schema/05_create_user_stickers.sql
+++ server/db/schema/05_create_user_stickers.sql
@@ -4,5 +4,7 @@ DROP TABLE IF EXISTS user_stickers CASCADE;
 CREATE TABLE user_stickers (
   id SERIAL PRIMARY KEY,
   user_id INT REFERENCES users(id),
-  sticker_id INT REFERENCES stickers(id)
+  sticker_id INT REFERENCES stickers(id),
+  title VARCHAR(255) NOT NULL,
+  active  BOOLEAN NOT NULL DEFAULT FALSE
 );
