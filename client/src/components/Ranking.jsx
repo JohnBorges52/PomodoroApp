c961d605ee6cbb089ac42075dcc5a45d11ea5442
john <jrborges52@gmail.com>
2022-09-13 18:08:46 -0700

    add the ranking system

--- client/src/components/Ranking.jsx
+++ client/src/components/Ranking.jsx
@@ -1,5 +1,7 @@
 import axios from "axios"
+import { useEffect } from "react"
 import { useState } from "react"
+import '../styles/ranking.scss'
 
 
 export const Ranking = () => {
@@ -11,11 +13,15 @@ export const Ranking = () => {
 
 
   const fecthTopUsers =  () => {
-      axios.get('pomodoros/ranking')
-      .then(res=>{console.log(res.data)
+    axios.get('pomodoros/ranking')
+    .then(res=>{setTopUsers(res.data)
+    })
 
-      })
   }
+  useEffect(()=>{
+    fecthTopUsers();
+  },[])
+  
 
 
   return (
@@ -23,7 +29,68 @@ export const Ranking = () => {
       <div className="ranking-title">Here is the list of the top users!</div>
       
       <div className="ranking-wrapper">
-        <button onClick={()=>{fecthTopUsers()}}>TEST</button>
+      <div className="ranking-subtitle"> 
+
+      <div className="no-place"> </div>
+
+      <div className="general-username">
+        Username
+      </div>
+     <div className="general-totaltime">Minutes</div>
+     </div>
+        {(topUsers.map((element) => {
+
+          if (topUsers.indexOf(element) === 0 ) {
+            return (
+              <div className="map-wrapper">  
+                <div className="first-place"> </div>
+                <div className="top-username-1">{element.username.substring(0,14)}</div>
+                <div className="top-totaltime">{element.total_time}</div>
+         
+              </div>
+            
+            )
+          } else if (topUsers.indexOf(element) === 1) {
+            return (
+              <div className="map-wrapper">  
+              
+              <div className="second-place"> </div>
+                <div className="top-username-2">{element.username.substring(0,14)}</div>
+                <div className="top-totaltime">{element.total_time}</div>
+         
+              </div>
+            
+            )
+          } else if (topUsers.indexOf(element) === 2) {
+            return (
+              <div className="map-wrapper">  
+              <div className="third-place"> </div>
+                <div className="top-username-3">{element.username.substring(0,14)}</div>
+                <div className="top-totaltime">{element.total_time}</div>
+         
+              </div>
+            
+            )
+          }
+          
+            return (
+              <div className="map-wrapper">  
+              
+              
+              <div className="no-place"> </div>
+                <div className="top-username">{element.username.substring(0,18)}</div>
+                <div className="top-totaltime">{element.total_time}</div>
+              
+
+              
+              </div>
+            
+  
+           
+            )
+
+        }))}
+        
         
 
       </div>
